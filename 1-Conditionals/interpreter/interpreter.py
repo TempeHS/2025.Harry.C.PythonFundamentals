0551edd9eec3c44fa0837df4fd9ba3739c7774d6
harryc73 <124547151+harryc73@users.noreply.github.com>
2024-02-14 02:50:13 +0000

    Upload completed interpreter.py

--- 1-Conditionals/interpreter/interpreter.py
+++ 1-Conditionals/interpreter/interpreter.py
@@ -1,19 +1,19 @@
 expression = input("Please place your equation here: ")
 x, y, z = expression.split(" ")
 
-float(x)
-float(z)
+x = float(x)
+z = float(z)
 
 
 if y == ("+"):
-    result = x + z
+    result = round(x + z, 1)
 elif y == ("*"):
-    result = x * z
+    result = round(x * z, 1)
 elif y == ("-"):
-    result = x - z
+    result = round(x - z, 1)
 elif y == ("/"):
-    result = x / z
+    result = round(x / z, 1)
 else:
     print("Unable to satisfy request")
 
-print(round(result, 1))
+print(result)
---
+++ 1-Conditionals/interpreter/interprettest.py
@@ -0,0 +1,8 @@
+expression = input("Please place your equation here: ")
+x, y, z = expression.split(" ")
+
+float(x)
+float(z)
+
+
+print(round(x + z, 1))
