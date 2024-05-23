534d9d9e5f1fc52ed88be35409ac26fb61f374e7
harryc73 <124547151+harryc73@users.noreply.github.com>
2024-05-23 14:17:42 +0000

    Upload semi done

--- 8-OOP/OOP/oop.py
+++ 8-OOP/OOP/oop.py
@@ -1,55 +1,73 @@
 class Shapes:
-    def __init__(
-        self,
-        colour,
-        height,
-        x,
-        y,
-        z,
-    ):
-        self.colour = colour
+    def __init__(self, height, x, y):
         self.x = x
         self.y = y
-        self.z = z
         self.height = height
 
 
 # See how turned area and radius into instance variables
 class Circle(Shapes):
 
-    def __init__(self, colour, height, x, y, z):
-        super().__init__(colour, height, x, y, z)
-        self.r = x / 2
-        self.a = self.r * self.r * 3.14
+    def __init__(
+        self,
+        height,
+        x,
+        y,
+    ):
+        super().__init__(
+            height,
+            x,
+            y,
+        )
+        self.radius = x / 2
+        self.area = self.radius * self.radius * 3.14
+        self.circ = 2 * 3.14 * self.radius
+        self.volume = 4 / 3 * 3.14 * self.radius * self.radius * self.radius
 
-    def circ_area(self):
-        return f"This is the {self.a}"
+    def get_circ_area(self):
+        return f"This is the {self.area}"
 
-    def circle_circ(self):
-        circ = 2 * 3.14 * self.r
-        return f"Ths is the circumference of a circle {circ}"
+    def get_circle_circ(self):
+        return f"Ths is the circumference of a circle {self.circ}"
 
-    def return_vol(self):
-        r = self.x / 2
-        r3 = r * r * r
-        v = 4 / 3 * 3.14 * r3
-        return f"The volume of the sphere is {v}"
+    def get_circ_vol(self):
+        return f"The volume of the sphere is: {self.volume}"
 
 
 # Put re-used code into initialisation
 class Triangle(Shapes):
-    def __init__(self, colour, height, x, y, z):
-        super().__init__(colour, height, x, y, z)
+    def __init__(self, height, x, y):
+        super().__init__(height, x, y)
+        self.area = 1 / 2 * self.x * self.y
+        self.volume = 1 / 3 * self.area * self.height
 
     def get_tri_area(self):
-        area = 1 / 2 * self.x * self.y
-        return f"This is the area: {area}"
+        return f"This is the area: {self.area}"
 
     def get_tri_volume(self):
-        area = 1 / 2 * self.x * self.y
-        v = 1 / 3 * area * self.height
-        return f"the volume is {v}"
+        return f"the volume is: {self.volume}"
 
 
-test = Circle("Red", 3, 5, 2, 9)
-print(test.circ_area())
+class Square(Shapes):
+    def __init__(self, height, x, y):
+        super().__init__(height, x, y)
+        self.area = self.x * self.y
+        self.volume = self.x * self.y * self.height
+
+    def get_square_area(self):
+        return f"The area is: {self.area}"
+
+    def get_square_volume(self):
+        return f"The square volume is: {self.volume}"
+
+
+class Move:
+    def __init__(self, colour, x, y, z):
+        self.colour = colour
+        self.x = x
+        self.y = y
+        self.z = z
+
+
+test = Circle(3, 5, 9)
+print(test.get_circ_vol())
